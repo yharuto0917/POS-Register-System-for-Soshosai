b0c8deaa234da5c6c2320fa10758f892f903da5d
yharuto0917 <yamazakiharuto.09171700@gmail.com>
2025-07-14 23:41:50 +0900

    secret_manager.pyにGEMINI_API_KEYを取得する記述を追加

--- functions/main.py
+++ functions/main.py
@@ -1,8 +1,5 @@
 from firebase_functions import https_fn
-from firebase_functions.options import set_global_options
 from firebase_admin import initialize_app
-
-set_global_options(max_instances=10)
 
 initialize_app()
 
