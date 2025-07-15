2cbab31f9a3c0fa4375b5344d1ad0af99e4969b2
yharuto0917 <yamazakiharuto.09171700@gmail.com>
2025-07-15 10:04:18 +0900

    functionsのエントリーポイントとなるmain.pyを作成しました

--- functions/main.py
+++ functions/main.py
@@ -3,7 +3,37 @@ from firebase_admin import initialize_app
 
 initialize_app()
 
+@https_fn.onCall()
+def on_call_backend(req: https_fn.CallableContext) -> https_fn.Response:
 
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+    if(req.data is None):
+        print("リクエストデータが存在しません[req.data is None in functions/main.py]")
+        raise https_fn.HttpsError(
+            code = https_fn.HttpsError.INVALID_ARGUMENT,
+            message = "リクエストデータが存在しません[req.data is None in functions/main.py]"
+        )
+
+    function_name = req.data.get("function_name")
+
+    if(function_name is None):
+        print("function_nameが存在しません[function_name is None in functions/main.py]")
+        raise https_fn.HttpsError(
+            code = https_fn.HttpsError.INVALID_ARGUMENT,
+            message = "function_nameが存在しません[function_name is None in functions/main.py]"
+        )
+    
+    if(function_name == "sendErrorLog"):
+        result = sedErroLog(req.data.contents)
+        return https_fn.Response(result)
+    elif(function_name == "geminiAnalyze"):
+        result = geminiAnalyze(req.data.contents)
+        return https_fn.Response(result)
+    elif(function_name == "excuteGAS"):
+        result = excuteGAS(req.data.contents)
+        return https_fn.Response(result)
+    else:
+        print("function_nameが存在しません[function_name is None in functions/main.py]")
+        raise https_fn.HttpsError(
+            code = https_fn.HttpsError.INVALID_ARGUMENT,
+            message = "function_nameが存在しません[function_name is None in functions/main.py]"
+        )
